199c6146f160134a2415e108058d54a2f0005335
jishnu <jishnusurajila@gmail.com>
2020-05-09 12:58:10 +0530

    some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import CustomButton from "./Components/customButton";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
+import DemoButton from "./Components/demoButton";
+import customButton from "./Components/customButton";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <CustomButton />
     </div>
   );
--- src/Components/customButton.js
+++ src/Components/customButton.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Content from "./Content";
 
 export default class customButton extends Component {
   constructor() {
@@ -6,8 +8,20 @@ export default class customButton extends Component {
     this.state = {
       color: "white",
       height: 50,
+      width: 40,
+      radius: 10,
     };
   }
+  changeRadius = (event) => {
+    this.setState({
+      radius: event.target.valueAsNumber,
+    });
+  };
+  changeTag = (event) => {
+    this.setState({
+      color: event.target.value,
+    });
+  };
   changeHeight = (event) => {
     this.setState({
       height: event.target.valueAsNumber,
@@ -18,41 +32,72 @@ export default class customButton extends Component {
       color: event.target.value,
     });
   };
+  changeWidth = (event) => {
+    this.setState({
+      width: event.target.valueAsNumber,
+    });
+  };
   render() {
     return (
       <Fragment>
-        <div className="properties">
-          {" "}
-          <ul style={{ listStyle: "none" }}>
-            <li>
-              <label>height :</label>
-              <input
-                type="number"
-                value={this.state.height}
-                onChange={this.changeHeight}
-              />
-            </li>
-            <li>
-              <label>Color:</label>
-              <input
-                type="text"
-                value={this.state.color}
-                onChange={this.changeTag}
-              />
-            </li>
-          </ul>
-        </div>
-        <button
-          style={{
-            alignItems: "center",
-            height: this.state.height,
-            backgroundColor: this.state.color,
-            marginTop: 100,
-            marginLeft: 200,
-          }}
-        >
-          click
-        </button>
+        <Container>
+          <Col>
+            {" "}
+            <div className="properties">
+              {" "}
+              <ul style={{ listStyle: "none" }}>
+                <li>
+                  <label>height :</label>
+                  <input
+                    type="number"
+                    value={this.state.height}
+                    onChange={this.changeHeight}
+                  />
+                </li>
+                <li>
+                  <label>width :</label>
+                  <input
+                    type="number"
+                    value={this.state.width}
+                    onChange={this.changeWidth}
+                  />
+                </li>
+                <li>
+                  <label>radius :</label>
+                  <input
+                    type="number"
+                    value={this.state.radius}
+                    onChange={this.changeRadius}
+                  />
+                </li>{" "}
+                <li>
+                  <label>Color:</label>
+                  <input
+                    type="text"
+                    value={this.state.color}
+                    onChange={this.changeTag}
+                  />
+                </li>
+              </ul>
+            </div>
+          </Col>
+          <Col>
+            <button
+              style={{
+                display: "inline-block",
+                alignItems: "center",
+                height: this.state.height,
+                width: this.state.width,
+                borderRadius: this.state.radius,
+                backgroundColor: this.state.color,
+                marginTop: 100,
+                marginLeft: 200,
+              }}
+            >
+              click
+            </button>
+          </Col>
+        </Container>
       </Fragment>
     );
   }
